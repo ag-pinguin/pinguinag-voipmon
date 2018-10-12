4542e699936407fcad80745a664691abd60af8b1
mzhaase <accounts@mattis-haase.de>
2018-10-12 13:23:53 +0200

    Use symlinks for ioncube installation

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -74,9 +74,8 @@ class voipmonitor::server::install(
   exec { 'install IONcube':
     command => '/usr/bin/wget http://voipmonitor.org/ioncube/x86_64/ioncube_loader_lin_7.0.so -O /usr/lib/php/20151012/ioncube_loader_lin_7.0.so \
     && /bin/echo "zend_extension = /usr/lib/php/20151012/ioncube_loader_lin_7.0.so" > /etc/php/7.0/mods-available/ioncube.ini \
-    && /bin/echo "zend_extension = /usr/lib/php/20151012/ioncube_loader_lin_7.0.so" > /etc/php/7.0/apache2/conf.d/01-ioncube.ini \
-    && /bin/echo "zend_extension = /usr/lib/php/20151012/ioncube_loader_lin_7.0.so" > /etc/php/7.0/cli/conf.d/01-ioncube.ini \
     && /bin/ln -s /etc/php/7.0/mods-available/ioncube.ini /etc/php/7.0/apache2/conf.d/01-ioncube.ini \
+    && /bin/ln -s /etc/php/7.0/mods-available/ioncube.ini /etc/php/7.0/fpm/conf.d/01-ioncube.ini \
     && /bin/ln -s /etc/php/7.0/mods-available/ioncube.ini /etc/php/7.0/cli/conf.d/01-ioncube.ini',
     creates => '/usr/lib/php/20151012/ioncube_loader_lin_7.0.so',
   }
